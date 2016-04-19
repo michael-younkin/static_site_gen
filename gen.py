82e335c1affde59a1741e9a7332ef6b9f4c8221b
Michael Younkin <younkin.michael@gmail.com>
2016-04-19 16:36:13 -0400

    Put actual blog data into a "data" dir

--- gen.py
+++ gen.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python3
 
+import os
+import os.path
 import shutil
 
-assert __name__ == '__main__'
+assert __name__ == "__main__"
 
-SRC = 'src/'
-OUT = 'out/'
+src_dir = "src"
+data_dir = os.path.join(src_dir, "data")
+out_dir = "out"
 
 try:
-    shutil.rmtree(OUT)
+    shutil.rmtree(out_dir)
 except FileNotFoundError:
     pass
-shutil.copytree(SRC, OUT)
+
+os.mkdir(out_dir)
+
+for root, dirs, files in os.walk(data_dir):
+    for f in files:
+        path = os.path.join(root, f)
+        dest = os.path.join(out_dir, path[len(data_dir) + 1:])
+        print("Copying", os.path.join(root, f), "to", dest)
+        os.makedirs(os.path.dirname(dest), exist_ok=True)
+        shutil.copy(path, dest)
