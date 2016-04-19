a61de119e195ffdbe354bd4b1f901ac04f747c21
Michael Younkin <younkin.michael@gmail.com>
2016-04-19 16:46:16 -0400

    Compile and copy over stylesheet

--- gen.py
+++ gen.py
@@ -3,12 +3,15 @@
 import os
 import os.path
 import shutil
+import subprocess
 
 assert __name__ == "__main__"
 
 src_dir = "src"
 data_dir = os.path.join(src_dir, "data")
+stylesheet = os.path.join(src_dir, "stylesheets", "main.scss")
 out_dir = "out"
+out_stylesheet = os.path.join(out_dir, "css", "main.css")
 
 try:
     shutil.rmtree(out_dir)
@@ -17,6 +20,7 @@ except FileNotFoundError:
 
 os.mkdir(out_dir)
 
+# Copy data directory
 for root, dirs, files in os.walk(data_dir):
     for f in files:
         path = os.path.join(root, f)
@@ -24,3 +28,7 @@ for root, dirs, files in os.walk(data_dir):
         print("Copying", os.path.join(root, f), "to", dest)
         os.makedirs(os.path.dirname(dest), exist_ok=True)
         shutil.copy(path, dest)
+
+# Compile SCSS
+os.makedirs(os.path.dirname(out_stylesheet), exist_ok=True)
+subprocess.check_call(["sass", stylesheet, out_stylesheet])
