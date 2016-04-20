c393c4231c4d43ed462cdc5f030231a63a5c509a
Michael Younkin <younkin.michael@gmail.com>
2016-04-20 00:17:31 -0400

    Add markdown support

--- gen.py
+++ gen.py
@@ -4,8 +4,11 @@ import os
 import os.path
 import shutil
 import subprocess
+import markdown
 
 assert __name__ == "__main__"
+
+md = markdown.Markdown(extensions = ['markdown.extensions.meta'])
 
 src_dir = "src"
 data_dir = os.path.join(src_dir, "data")
@@ -24,11 +27,23 @@ os.mkdir(out_dir)
 for root, dirs, files in os.walk(data_dir):
     for f in files:
         path = os.path.join(root, f)
+        print("File:", path)
         dest = os.path.join(out_dir, path[len(data_dir) + 1:])
-        print("Copying", os.path.join(root, f), "to", dest)
+        print("\tDest:", dest)
         os.makedirs(os.path.dirname(dest), exist_ok=True)
-        shutil.copy(path, dest)
+
+        ext = os.path.splitext(f)[1]
+        if ext == ".mkd":
+            print("\tParsing markdown")
+            text = open(path).read()
+            html = md.convert(text)
+            open(dest, mode='w').write(html)
+        else:
+            print("\tCopying")
+            shutil.copy(path, dest)
 
 # Compile SCSS
+print("Compile SCSS:", stylesheet)
 os.makedirs(os.path.dirname(out_stylesheet), exist_ok=True)
 subprocess.check_call(["sass", stylesheet, out_stylesheet])
+print("\tOk")
