12d004632b2912493e2c6ef7878ad7835e78b7a6
Michael Younkin <younkin.michael@gmail.com>
2016-04-29 23:17:11 -0400

    Load and render blog posts

--- gen.py
+++ gen.py
@@ -2,11 +2,12 @@
 
 assert __name__ == "__main__"
 
-import glob
+from glob import glob
 import os
 import os.path
 import shutil
 import subprocess
+from markdown import Markdown
 
 from jinja2 import Environment, FileSystemLoader
 
@@ -43,3 +44,28 @@ env = Environment(loader=FileSystemLoader(SRC_DIR))
 # Render index.html
 print("Render and write index.html")
 open("out/index.html", "w").write(env.get_template("data/index.jinja").render())
+
+# Load all blog posts
+md = Markdown()
+
+class BlogPost:
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.html = md.convert(open(filename).read())
+
+blog_posts = {}
+for blog_file in glob("src/data/blog/*.mkd"):
+    blog_posts[blog_file] = BlogPost(blog_file)
+
+# Make blog output directory
+os.mkdir("out/blog")
+
+# Render and save blog posts
+for post in blog_posts.values():
+    context = {"post_title": post.filename, "post_body": post.html}
+    post_docs = env.get_template("templates/blog_post.jinja").render(context)
+    name = os.path.splitext(os.path.basename(post.filename))[0]
+    out_filename = "out/blog/%s.html" % name
+    print("Render and write", out_filename)
+    open(out_filename, "w").write(post_docs)
